67e73c8b261346e18dfe122ff1b02c9c9cce1c3d
jph-sw <jph.sw@proton.me>
2025-10-14 18:03:46 +0000

    feat: use own db (idk if that's a good idea)

--- server.ts
+++ server.ts
@@ -2,40 +2,12 @@ import express from "express";
 import expressWebsockets from "express-ws";
 import { Hocuspocus } from "@hocuspocus/server";
 import { Database } from "@hocuspocus/extension-database";
+import { SQLite } from "@hocuspocus/extension-sqlite";
 
 const hocuspocus = new Hocuspocus({
   extensions: [
-    new Database({
-      fetch: async ({ documentName }) => {
-        const res = await fetch(
-          `${process.env.YAN_URL}/api/get-document/${documentName}`,
-        );
-        const data = (await res.json()) as any;
-
-        console.log("API Response:", JSON.stringify(data, null, 2));
-
-        if (!data || typeof data.doc !== "string") {
-          throw new Error(
-            `Invalid document data structure received from API: ${JSON.stringify(data)}`,
-          );
-        }
-
-        const binaryData = Buffer.from(data.doc, "base64");
-
-        return binaryData;
-      },
-      store: async ({ documentName, state }) => {
-        const res = await fetch(
-          `${process.env.YAN_URL}/api/set-document/${documentName}`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/octet-stream",
-            },
-            body: state,
-          },
-        );
-      },
+    new SQLite({
+      database: "local.db",
     }),
   ],
 });
